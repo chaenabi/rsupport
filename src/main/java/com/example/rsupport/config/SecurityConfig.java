013b8adc486716759d8f55a24f5fb107241a3d87
chaenabi <find1086@gmail.com>
2022-01-26 17:35:48 +0900

    Add Security: 스프링 시큐리티 설정 추가

---
+++ src/main/java/com/example/rsupport/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.rsupport.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@Configuration
+@Order(1)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().mvcMatchers("/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+//                .antMatchers("/test/role_member").hasRole("USER")
+//                .antMatchers("/test/role_admin").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .antMatchers("/v1/notice").permitAll();
+
+        http.csrf()
+                .disable();
+
+        http.headers()
+                .frameOptions()
+                .disable();
+
+        http.formLogin()
+                .disable();
+    }
+
+}
