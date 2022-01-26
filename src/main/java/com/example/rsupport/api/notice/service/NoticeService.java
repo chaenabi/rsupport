36b2d6deee88682fb6ab3c9858f02b1ed151c53b
chaenabi <find1086@gmail.com>
2022-01-26 17:34:26 +0900

    Add Notice Service: 공지사항 서비스 추가

---
+++ src/main/java/com/example/rsupport/api/notice/service/NoticeService.java
@@ -0,0 +1,12 @@
+package com.example.rsupport.api.notice.service;
+
+import com.example.rsupport.api.notice.domain.dto.NoticeRegisterRequestDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+
+    public Long registerNotice(NoticeRegisterRequestDTO dto) {
+        return 1L;
+    }
+}
