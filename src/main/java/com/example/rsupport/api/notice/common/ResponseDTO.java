d65e9dc5459fbf63ed616396d7890603c72c47ff
chaenabi <find1086@gmail.com>
2022-01-26 17:19:09 +0900

    Add ResponseDTO: 정형화된 응답포맷 추가

---
+++ src/main/java/com/example/rsupport/api/notice/common/ResponseDTO.java
@@ -0,0 +1,32 @@
+package com.example.rsupport.api.notice.common;
+
+import com.google.gson.Gson;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 컨트롤러에서 반환하는 결과 데이터를, 일관된 포맷형식으로 클라이언트에게 전달하기 위한 클래스
+ *
+ * @param <T> 컨트롤러에서 반환하고자 하는 데이터타입
+ *
+ * @author MC Lee
+ * @since 2.6.3 spring boot
+ * @since 0.0.1 dev
+ */
+@Getter
+public class ResponseDTO<T> {
+    private final T data;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public ResponseDTO(T data, SuccessMessage message, HttpStatus httpStatus) {
+        this.data = data;
+        this.message = message.getSuccessMsg();
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
