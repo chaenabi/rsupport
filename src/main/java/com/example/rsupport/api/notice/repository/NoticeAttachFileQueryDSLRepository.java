a868525cc54ca3d3c8536bdac0b22dc633de6312
chaenabi <find1086@gmail.com>
2022-01-28 21:56:35 +0900

    Cleanup Repository code: 불필요한 레포지토리 테스트 삭제, 빠진 정보 추가

--- src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileQueryDSLRepository.java
+++ src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileQueryDSLRepository.java
@@ -1,9 +1,11 @@
 package com.example.rsupport.api.notice.repository;
 
 import com.example.rsupport.api.notice.domain.entity.NoticeAttachFile;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface NoticeAttachFileQueryDSLRepository {
     List<NoticeAttachFile> findByNoticeId(Long noticeId);
 }
--- src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileRepositoryImpl.java
+++ src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.example.rsupport.api.notice.repository;
 import com.example.rsupport.api.notice.domain.entity.NoticeAttachFile;
 import com.example.rsupport.api.notice.domain.entity.QNoticeAttachFile;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,10 +13,11 @@ public class NoticeAttachFileRepositoryImpl implements NoticeAttachFileQueryDSLR
 
     @PersistenceContext
     private EntityManager entityManager;
-    private JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+    private JPAQueryFactory queryFactory;
 
     @Override
     public List<NoticeAttachFile> findByNoticeId(Long noticeId) {
+        queryFactory = new JPAQueryFactory(entityManager);
         final QNoticeAttachFile qAttachFile = QNoticeAttachFile.noticeAttachFile;
         return queryFactory.selectFrom(qAttachFile)
                 .where(qAttachFile.notice.id.eq(noticeId))
