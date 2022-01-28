d2735933613897376e85f1028a7b017ffedbbf8c
chaenabi <find1086@gmail.com>
2022-01-28 15:45:08 +0900

    Add NoticeMessage: 공지사항 관련 처리성공 메시지 추가

--- src/main/java/com/example/rsupport/api/notice/domain/enums/NoticeMessage.java
+++ src/main/java/com/example/rsupport/api/notice/domain/enums/NoticeMessage.java
@@ -1,11 +1,14 @@
 package com.example.rsupport.api.notice.domain.enums;
 
-import com.example.rsupport.api.notice.common.SuccessMessage;
+import com.example.rsupport.api.common.SuccessMessage;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum NoticeMessage implements SuccessMessage {
-    SUCCESS_REGISTER_NOTICE("공지사항 등록이 성공적으로 완료되었습니다.");
+    SUCCESS_NOTICE_REGISTER("공지사항 등록이 성공적으로 완료되었습니다."),
+    SUCCESS_NOTICE_UPDATE("공지사항 수정이 성공적으로 완료되었습니다."),
+    SUCCESS_NOTICE_DELETE("공지사항 수정이 성공적으로 완료되었습니다."),
+    SUCCESS_NOTICE_SELECTONE("공지사항 단건 조회가 성공적으로 완료되었습니다.");
 
     private final String successMsg;
 
