0178df8e3a2b48105d0199609fecb3f7d36b123c
chaenabi <find1086@gmail.com>
2022-01-28 15:45:46 +0900

    Add Notice Entity: 공지사항 엔티티 추가

--- src/main/java/com/example/rsupport/api/notice/domain/entity/Notice.java
+++ src/main/java/com/example/rsupport/api/notice/domain/entity/Notice.java
@@ -1,13 +1,49 @@
 package com.example.rsupport.api.notice.domain.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static javax.persistence.CascadeType.PERSIST;
+import static javax.persistence.CascadeType.REMOVE;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.AUTO;
 
 @Entity
+@Table
+@Getter
+@Setter
+@ToString(exclude = "attachFile")
 public class Notice {
 
     @Id
+    @GeneratedValue(strategy = AUTO)
     @Column(name = "notice_id")
     private Long id;
+
+    private String title;
+    private String content;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    @OneToMany(mappedBy = "notice", cascade = {PERSIST, REMOVE}, orphanRemoval = true, fetch = LAZY)
+    private List<NoticeAttachFile> attachFile;
+
+    public Notice() {
+    }
+
+    @Builder
+    public Notice(Long id, String title, String content, LocalDateTime startTime, LocalDateTime endTime, List<NoticeAttachFile> attachFile) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.attachFile = attachFile;
+    }
 }
