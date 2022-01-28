7d3cdcffb91dbd30fca25fa864eefd31bee7a7c8
chaenabi <find1086@gmail.com>
2022-01-28 15:47:53 +0900

    Impl NoticeAttachFileRepositoryImpl: 공지사항 레포지토리 인터페이스 구현(queryDSL)

--- src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileRepositoryImpl.java
+++ src/main/java/com/example/rsupport/api/notice/repository/NoticeAttachFileRepositoryImpl.java
@@ -1,2 +1,24 @@
-package com.example.rsupport.api.notice.repository;public class NoticeAttachFileRepositoryImpl {
+package com.example.rsupport.api.notice.repository;
+
+import com.example.rsupport.api.notice.domain.entity.NoticeAttachFile;
+import com.example.rsupport.api.notice.domain.entity.QNoticeAttachFile;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+public class NoticeAttachFileRepositoryImpl implements NoticeAttachFileQueryDSLRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    private JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+
+    @Override
+    public List<NoticeAttachFile> findByNoticeId(Long noticeId) {
+        final QNoticeAttachFile qAttachFile = QNoticeAttachFile.noticeAttachFile;
+        return queryFactory.selectFrom(qAttachFile)
+                .where(qAttachFile.notice.id.eq(noticeId))
+                .fetch();
+    }
 }
