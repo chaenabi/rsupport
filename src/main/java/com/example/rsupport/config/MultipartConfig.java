374f3f9daeb5353e53d6ce285bcdba5ae1005d89
chaenabi <find1086@gmail.com>
2022-01-28 15:32:30 +0900

    Edit MultipartConfig: 멀티파트 리졸버 설정변경

--- src/main/java/com/example/rsupport/config/MultipartConfig.java
+++ src/main/java/com/example/rsupport/config/MultipartConfig.java
@@ -2,19 +2,17 @@ package com.example.rsupport.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MultipartConfig implements WebMvcConfigurer {
 
-    private final long MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024;
+    private final long MAX_FILE_SIZE = 10 * 1024 * 1024;
 
     @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-        multipartResolver.setDefaultEncoding("UTF-8");
-        multipartResolver.setMaxUploadSizePerFile(MAX_UPLOAD_FILE_SIZE);
+    public StandardServletMultipartResolver multipartResolver() {
+        StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
         return multipartResolver;
     }
 }
