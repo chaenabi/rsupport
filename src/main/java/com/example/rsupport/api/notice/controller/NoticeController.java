d584073f0c053ee0e9482428d272b27f7131fc5f
chaenabi <find1086@gmail.com>
2022-01-28 15:44:41 +0900

    Impl NoticeController: 공지사항 컨트롤러 로직 구현

--- src/main/java/com/example/rsupport/api/notice/controller/NoticeController.java
+++ src/main/java/com/example/rsupport/api/notice/controller/NoticeController.java
@@ -1,7 +1,8 @@
 package com.example.rsupport.api.notice.controller;
 
-import com.example.rsupport.api.notice.common.ResponseDTO;
+import com.example.rsupport.api.common.ResponseDTO;
 import com.example.rsupport.api.notice.domain.dto.NoticeRegisterRequestDTO;
+import com.example.rsupport.api.notice.domain.dto.NoticeUpdateRequestDTO;
 import com.example.rsupport.api.notice.domain.enums.NoticeMessage;
 import com.example.rsupport.api.notice.service.NoticeService;
 import com.example.rsupport.exception.notice.InvalidNoticeParameterException;
@@ -10,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -25,9 +26,26 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @PostMapping("/notice")
-    public ResponseDTO<Long> registerNotice(@Valid @RequestBody NoticeRegisterRequestDTO dto, BindingResult result) {
+    @PostMapping(value = "/notice")
+    public ResponseDTO<Long> registerNotice(@Valid @RequestPart(value = "data") NoticeRegisterRequestDTO dto,
+                                            BindingResult result,
+                                            @RequestPart(value = "attachFiles", required = false) List<MultipartFile> attachFiles
+    ) {
         if (result.hasErrors()) throw new InvalidNoticeParameterException(result, NoticeCrudErrorCode.NOTICE_CRUD_FAIL);
-        return new ResponseDTO<>(noticeService.registerNotice(dto), NoticeMessage.SUCCESS_REGISTER_NOTICE, HttpStatus.OK);
+        if (attachFiles == null || attachFiles.size() == 0) attachFiles = Collections.emptyList();
+        return new ResponseDTO<>(noticeService.registerNotice(dto, attachFiles), NoticeMessage.SUCCESS_NOTICE_REGISTER, HttpStatus.OK);
     }
+
+    @PatchMapping("/notice")
+    public ResponseDTO<Long> updateNotice(@Valid @RequestBody NoticeUpdateRequestDTO dto, BindingResult result) {
+        if (result.hasErrors()) throw new InvalidNoticeParameterException(result, NoticeCrudErrorCode.NOTICE_CRUD_FAIL);
+        return new ResponseDTO<>(noticeService.updateNotice(dto), NoticeMessage.SUCCESS_NOTICE_UPDATE, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/notice/{noticeId}")
+    public ResponseDTO<Void> removeNotice(@PathVariable Long noticeId) {
+        noticeService.removeNotice(noticeId);
+        return new ResponseDTO<>(NoticeMessage.SUCCESS_NOTICE_UPDATE, HttpStatus.OK);
+    }
+
 }
