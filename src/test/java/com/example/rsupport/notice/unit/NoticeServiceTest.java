ca03414538cb16ce23e656ba07bb890fc95dac4b
chaenabi <find1086@gmail.com>
2022-01-29 17:32:55 +0900

    Add NoticeServiceTest: 공지사항 수정,삭제,조회 테스트 추가

--- src/test/java/com/example/rsupport/notice/unit/NoticeServiceTest.java
+++ src/test/java/com/example/rsupport/notice/unit/NoticeServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.rsupport.notice.unit;
 
+import com.example.rsupport.api.notice.domain.dto.NoticeDTO;
 import com.example.rsupport.api.notice.domain.dto.NoticeRegisterRequestDTO;
+import com.example.rsupport.api.notice.domain.dto.NoticeUpdateRequestDTO;
 import com.example.rsupport.api.notice.domain.entity.Notice;
 import com.example.rsupport.api.notice.domain.entity.NoticeAttachFile;
 import com.example.rsupport.api.notice.repository.NoticeAttachFileRepository;
@@ -21,6 +23,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.apache.commons.io.FilenameUtils.getBaseName;
@@ -140,16 +143,97 @@ public class NoticeServiceTest {
     @DisplayName("공지사항 수정 테스트")
     class NoticeUpdateTest {
 
+        private final Long wantToUpdateNoticeId = 1L;
+
+        private final NoticeUpdateRequestDTO dto = NoticeUpdateRequestDTO
+                .builder()
+                .noticeId(wantToUpdateNoticeId)
+                .title("수정된 공지사항 제목")
+                .content("수정된 공지사항 내용")
+                .build();
+
+        private final Notice wantToUpdateNotice = Notice.builder()
+                .id(wantToUpdateNoticeId)
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .build();
+
+        @BeforeEach
+        void setUp() {
+            String DEFAULT_UPLOAD_DIRECTORY = "src/main/resources/upload";
+            LocalDateTime now = LocalDateTime.now();
+            String randomString = UUID.randomUUID().toString().split("-")[0];
+
+            attachFiles.forEach(file -> files.add(NoticeAttachFile.builder()
+                    .filename(DEFAULT_UPLOAD_DIRECTORY
+                            + "/"
+                            + getBaseName(file.getOriginalFilename())
+                            + "_"
+                            + now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+                            + "_"
+                            + randomString
+                            + "."
+                            + getExtension(file.getOriginalFilename())
+                    )
+                    .notice(wantToUpdateNotice)
+                    .build()));
+        }
+
+        File file1 = new File("src/test/resources/avatar.jpg");
+        File file2 = new File("src/test/resources/notice.txt");
+        MultipartFile attachFile1 = new MockMultipartFile("attachFiles", file1.getName(), MULTIPART_FORM_DATA_VALUE, file1.getName().getBytes());
+        MultipartFile attachFile2 = new MockMultipartFile("attachFiles", file2.getName(), MULTIPART_FORM_DATA_VALUE, file2.getName().getBytes());
+        List<MultipartFile> attachFiles = List.of(attachFile1, attachFile2);
+        List<NoticeAttachFile> files = new ArrayList<>();
+
         @Test
         @DisplayName("공지사항 수정 성공")
+        @SuppressWarnings("all")
         void successUpdateNotice() {
-            
+
+            // 준비
+            given(noticeRepository.findById(wantToUpdateNoticeId)).willReturn(Optional.of(wantToUpdateNotice));
+            doNothing().when(noticeAttachFileRepository).deleteAll(anyList());
+            doNothing().when(noticeAttachFileRepository).flush();
+            given(noticeAttachFileRepository.saveAll(anyList())).willReturn(files);
+
+            // 실행
+            Long noticeId = noticeService.updateNotice(dto, attachFiles);
+
+            // 검증
+            verify(noticeRepository, atMostOnce()).findById(refEq(wantToUpdateNoticeId));
+            verify(noticeAttachFileRepository, atMostOnce()).deleteAll(files);
+            verify(noticeAttachFileRepository, atMostOnce()).flush();
+            verify(noticeAttachFileRepository, atMostOnce()).saveAll(files);
+            assertThat(noticeId).isEqualTo(wantToUpdateNoticeId);
+            files.clear();
         }
 
         @Test
-        @DisplayName("공지사항 수정 실패")
-        void failUpdateNotice() {
+        @DisplayName("공지사항 수정 실패_공지사항이 존재하지 않음")
+        void failUpdateNotice_No_Exist_Notice() {
+            // 준비
+            doThrow(new BizException(NoticeCrudErrorCode.NOTICE_NOT_FOUND)).when(noticeRepository).findById(wantToUpdateNoticeId);
 
+            // 실행 및 검증
+            assertThatThrownBy(() -> noticeRepository.findById(wantToUpdateNoticeId))
+                    .isInstanceOf(BizException.class)
+                    .hasMessage(NoticeCrudErrorCode.NOTICE_NOT_FOUND.getMsg());
+            files.clear();
+        }
+
+        @Test
+        @DisplayName("공지사항 수정 실패_첨부파일 저장 실패")
+        @SuppressWarnings("unchecked")
+        void failUpdateNotice_Save_AttachFile_Fail() {
+            // 준비
+            doThrow(new BizException(NoticeCrudErrorCode.NOTICE_CRUD_FAIL)).when(savingAttachFile).saveUploadFilesToDisk(any(List.class), refEq(wantToUpdateNotice));
+
+            // 실행 및 검증
+            assertThatThrownBy(() -> savingAttachFile.saveUploadFilesToDisk(attachFiles, wantToUpdateNotice))
+                    .isInstanceOf(BizException.class)
+                    .hasMessage(NoticeCrudErrorCode.NOTICE_CRUD_FAIL.getMsg());
+            files.clear();
         }
     }
 
@@ -157,16 +241,37 @@ public class NoticeServiceTest {
     @DisplayName("공지사항 삭제 테스트")
     class NoticeDeleteTest {
 
+        private final Long wantToDeleteNoticeId = 1L;
+
         @Test
         @DisplayName("공지사항 삭제 성공")
         void successDeleteNotice() {
+            // 준비
+            Notice findNoticeToDelete = Notice.builder()
+                    .id(wantToDeleteNoticeId)
+                    .title("삭제될 공지사항의 제목")
+                    .content("삭제될 공지사항의 내용")
+                    .build();
 
+            given(noticeRepository.findById(1L)).willReturn(Optional.of(findNoticeToDelete));
+            doNothing().when(noticeRepository).deleteById(wantToDeleteNoticeId);
+
+            // 실행
+            verify(noticeRepository, atMostOnce()).deleteById(wantToDeleteNoticeId);
         }
 
         @Test
         @DisplayName("공지사항 삭제 실패")
         void failDeleteNotice() {
+            // 준비
+            doThrow(new BizException(NoticeCrudErrorCode.NOTICE_NOT_FOUND))
+                    .when(noticeRepository)
+                    .deleteById(wantToDeleteNoticeId);
 
+            // 실행 및 검증
+            assertThatThrownBy(() -> noticeRepository.deleteById(wantToDeleteNoticeId))
+                    .isInstanceOf(BizException.class)
+                    .hasMessage(NoticeCrudErrorCode.NOTICE_NOT_FOUND.getMsg());
         }
     }
 
@@ -174,16 +279,55 @@ public class NoticeServiceTest {
     @DisplayName("공지사항 단건 조회 테스트")
     class NoticeSelectOneTest {
 
+        private final Long wantToSelectNoticeId = 1L;
+
         @Test
         @DisplayName("공지사항 단건 조회 성공")
         void successSelectOneNotice() {
+            // 준비
+            Notice findOneNotice = Notice.builder()
+                    .id(wantToSelectNoticeId)
+                    .title("조회될 공지사항의 제목")
+                    .content("조회될 공지사항의 내용")
+                    .startTime(LocalDateTime.now())
+                    .endTime(LocalDateTime.now().plusDays(30))
+                    .sawCount(10)
+                    .build();
 
+            given(noticeRepository.findById(1L)).willReturn(Optional.of(findOneNotice));
+
+            NoticeDTO result = NoticeDTO.builder()
+                    .noticeId(findOneNotice.getId())
+                    .title(findOneNotice.getTitle())
+                    .content(findOneNotice.getContent())
+                    .startTime(findOneNotice.getStartTime())
+                    .endTime(findOneNotice.getEndTime())
+                    .sawCount(findOneNotice.getSawCount())
+                    .build();
+
+            // 실행
+            NoticeDTO noticeDTO = noticeService.selectNoticeOne(wantToSelectNoticeId);
+
+            assertThat(noticeDTO).extracting("noticeId").isEqualTo(result.getNoticeId());
+            assertThat(noticeDTO).extracting("title").isEqualTo(result.getTitle());
+            assertThat(noticeDTO).extracting("content").isEqualTo(result.getContent());
+            assertThat(noticeDTO).extracting("startTime").isEqualTo(result.getStartTime());
+            assertThat(noticeDTO).extracting("endTime").isEqualTo(result.getEndTime());
+            assertThat(noticeDTO).extracting("sawCount").isEqualTo(result.getSawCount() + 1);
         }
 
         @Test
         @DisplayName("공지사항 단건 조회 실패")
         void failSelectOneNotice() {
+            // 준비
+            doThrow(new BizException(NoticeCrudErrorCode.NOTICE_NOT_FOUND))
+                    .when(noticeRepository)
+                    .findById(wantToSelectNoticeId);
 
+            // 실행 및 검증
+            assertThatThrownBy(() -> noticeRepository.findById(wantToSelectNoticeId))
+                    .isInstanceOf(BizException.class)
+                    .hasMessage(NoticeCrudErrorCode.NOTICE_NOT_FOUND.getMsg());
         }
     }
 }
